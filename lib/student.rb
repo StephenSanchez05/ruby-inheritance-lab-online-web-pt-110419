b24bf2cb0bb9dc2a14d65255922bd2f34e0959e3
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-01 01:56:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student < User
 
+attr_accessor :knowledge
+
+def initialize
+  @knowledge = []
+end
+
+def learn
+  @knowledge << Teacher.teach
+end
+
+def knowledge
+  @knowledge
+end
+
 end
